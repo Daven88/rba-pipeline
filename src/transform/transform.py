2da487c2368ed02ab7d05ac8e23819f81cfd95dc
David Northey <dnorthey88@gmail.com>
2026-04-28 20:03:24 +1000

    Add transform script and update requirements

---
+++ src/transform/transform.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from google.cloud import storage
+import pyarrow
+import io
+import json
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timezone, date
+
+def read_from_bronze(bucket_name, blob):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob_loc = bucket.blob(blob)
+    json_data = blob_loc.download_as_bytes()
+    data = json.loads(json_data)
+    return data
+
+def clean_data(payload):
+
+    records = payload['records']
+    df = pd.DataFrame(records)
+    df['indicator_id'] = df['indicator'].apply(lambda x:x['id']) 
+    df['indicator_name'] = df['indicator'].apply(lambda x:x['value'])                                                                                                             
+    df['country_id'] = df['country'].apply(lambda x:x['id']) 
+    df['country_name'] = df['country'].apply(lambda x:x['value'])  
+    df = df.drop(columns=['indicator', 'country', 'unit', 'obs_status', 'decimal'])
+    df['date'] = df['date'].astype(int)
+    return df
+
+def load_to_silver(df, silver_bucket):
+    buffer = io.BytesIO()                                                                                                                                                     
+    df.to_parquet(buffer)
+    buffer.seek(0)                                                                                                                                                            
+    client = storage.Client()                                                                                                                                                 
+    bucket_obj = client.bucket(silver_bucket)
+    blob_name = f'interest_rates/{date.today()}.parquet' 
+    blob_loc = bucket_obj.blob(blob_name) 
+    blob_loc.upload_from_string(buffer.getvalue(),content_type='application/octet-stream')
+    
+def main():
+
+    dot_env_path = os.path.join(os.path.dirname(__file__), '..', '..', 'config', '.env')
+    load_dotenv(dot_env_path)
+    bucket_name = os.getenv('GCS_BRONZE_BUCKET')
+    blob_loc = f'interest_rates/{date.today()}.json'
+
+    silver_bucket = os.getenv('GCS_SILVER_BUCKET')
+
+    extracted_data = read_from_bronze(bucket_name, blob_loc)
+
+    df = clean_data(extracted_data)
+
+    load_to_silver(df, silver_bucket)
+
+if __name__ == "__main__":
+    main()
